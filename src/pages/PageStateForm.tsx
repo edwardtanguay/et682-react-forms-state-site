17347bc21316c4113a7da4f9b418f6b60191c84e
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-14 10:37:52 +0100

    can change firstname

--- src/pages/PageStateForm.tsx
+++ src/pages/PageStateForm.tsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+
+const initialFormInfo = {
+	fields: {
+		firstName: {
+			label: "First Name",
+			value: "nnn222",
+			isRequired: true,
+			isValid: true,
+		},
+	},
+};
+
 export const PageStateForm = () => {
+	const [formInfo, setFormInfo] = useState(initialFormInfo);
+
+	const handleFieldFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value;
+		const _formInfo = structuredClone(formInfo);
+		_formInfo.fields.firstName.value = value;
+		setFormInfo(_formInfo);
+
+		// setFormInfo({...formInfo, value: e.target.value})
+
+		//  setFormInfo({...formInfo, fields.firstName: { ...formInfo.fields.firstName, value: e.target.value },
+// /   });
+	};
 	return (
-		<p>This is the state form page.</p>
-	)
-}
+		<form>
+			<fieldset>
+				<legend>New Member</legend>
+
+				<div>
+					[{formInfo.fields.firstName.value}]
+					<label htmlFor="firstName"></label>
+					<input
+						type="text"
+						onChange={(e) => handleFieldFirstName(e)}
+						value={formInfo.fields.firstName.value}
+						id="firstName"
+					/>
+				</div>
+				<button>Submit</button>
+			</fieldset>
+		</form>
+	);
+};
